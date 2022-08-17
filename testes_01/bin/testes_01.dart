1974d8c700c7ab5ab9c0cd61e30b06529137d690
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-08-17 08:59:00 -0300

    Iniciando repositório das atividades

---
+++ testes_01/bin/testes_01.dart
@@ -0,0 +1,5 @@
+import 'package:testes_01/testes_01.dart' as testes_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${testes_01.calculate()}!');
+}
---
+++ testes_01/test/testes_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:testes_01/testes_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
